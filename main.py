60f706a9d7c28d3895c5c53d7981cc77386eeef6
Tylxer <50160168+Tylxer@users.noreply.github.com>
2020-03-07 17:53:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from aiowebsocket.converses import AioWebSocket
 from requests.adapters import HTTPAdapter
 
 
-models = {'Kati3kat':'119647139','VivianisHere':'114190318','FancyVikki':'121523305','AvrilDollX':'118968133','Mila_Poonis':'127545784','MissAlice_94':'110256061','OneSweetBae':'121723980','Virgin_Emma':'126192483'}
+models = {'Kati3kat':'19647139','VivianisHere':'14190318','FancyVikki':'21523305','AvrilDollX':'18968133','Mila_Poonis':'27545784','MissAlice_94':'10256061','OneSweetBae':'21723980','Virgin_Emma':'26192483'}
 status =  {'Kati3kat':0,'VivianisHere':0,'FancyVikki':0,'AvrilDollX':0,'Mila_Poonis':0,'MissAlice_94':0,'OneSweetBae':0,'Virgin_Emma':0}#0表示未下载，1表示正在下载
 gLock = threading.Lock()
 
@@ -54,7 +54,7 @@ async def startup(uri):
                 print(fcw_url)
                 print(time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime()))
                 while True:
-                    fcw_data = request_get(fcw_url)
+                    fcw_data = request_get(fcw_url,5,5)
                     if fcw_data.status_code == 200:
                         fcw = fcw_data.text
                         break
@@ -63,21 +63,21 @@ async def startup(uri):
                     continue
                 config_url='https://www.myfreecams.com/_js/serverconfig.js'
                 while True:
-                    config_data = request_get(config_url)
+                    config_data = request_get(config_url,5,5)
                     if config_data.status_code == 200:
                         config = config_data.text
                         break
                 for model in models:
                     if status[model] == 0:
-                        if fcw.find(model) != -1:
+                        id = models.get(model)
+                        if fcw.find(id) != -1:
                             i = 0
                             while True:
-                                if fcw_json["rdata"][i][0] == model:
+                                if fcw_json["rdata"][i][2] == int(id):
                                     break
                                 else:
                                     i = i + 1
                             videoid = fcw_json["rdata"][i][6]
-                            print(str(videoid))
                             config_json = json.loads(config)
                             if str(videoid) in config_json['h5video_servers']:
                                 videoser = config_json["h5video_servers"][str(videoid)]
@@ -91,26 +91,28 @@ async def startup(uri):
                             else:
                                 print(model+'失败')
                                 continue
-                            test = '.myfreecams.com/NxServer/ngrp:mfc_'
-                            playlisturl = 'https://' + videoser + test + models.get(model) + '.f4v_mobile/playlist.m3u8'
-                            res = request_get(playlisturl)
+                            test = '.myfreecams.com/NxServer/ngrp:mfc_1'
+                            playlisturl = 'https://' + videoser + test + id + '.f4v_mobile/playlist.m3u8'
+                            res = request_get(playlisturl,15,5)
                             if 'http'  not in res.text:
-                                test = '.myfreecams.com/NxServer/ngrp:mfc_a_'
-                                playlisturl = 'https://' + videoser + test + models.get(model) + '.f4v_mobile/playlist.m3u8'
-                                res = request_get(playlisturl)
+                                test = '.myfreecams.com/NxServer/ngrp:mfc_a_1'
+                                playlisturl = 'https://' + videoser + test + id + '.f4v_mobile/playlist.m3u8'
+                                res = request_get(playlisturl,15,5)
                             if res.status_code == 200 and status[model] == 0:
                                 print(model + ' is online')
                                 chunklist = dels(res.text.splitlines())
                                 url = ''
+                                print(chunklist)
                                 for s in chunklist:
                                     if 'chunklist' in s:
                                         strlist = s.split('_')
                                         if '256' in strlist[-1]:                                          
-                                            url = 'https://' + videoser + test + models.get(model) + '.f4v_mobile/'+ s
+                                            url = 'https://' + videoser + test + id + '.f4v_mobile/'+ s
                                             break
                                         else:
-                                            url = 'https://' + videoser + test + models.get(model) + '.f4v_mobile/'+ s
-                                if 'http' in url:
+                                            url = 'https://' + videoser + test + id + '.f4v_mobile/'+ s
+                                print(url)
+                                if not url:
                                     ol_model.put(model)
                                     ol_url.put(url)
                                 else:
@@ -126,7 +128,7 @@ async def startup(uri):
 
 def geturl(url,num):
     downurl = queue.Queue()
-    res = request_get(url)
+    res = request_get(url,15,8)
     savenum = []
     if res.status_code == 200:
         temp = res.text.splitlines()
@@ -151,13 +153,13 @@ def dels(list):
 
 def maindown(url,model):
     print('准备下载'+model)
-    print(url)
+    #print(url)
     savenum = [0]
     sleepnum = 0
     filenum = []        
     file = temppath + model + '/'
     file1 = savepath + model + '/'
-    if request_get(url).status_code == 200:
+    if request_get(url,10,8).status_code == 200:
         request_post(model,'开始下载')
         status[model] = 1
         while True:
@@ -178,7 +180,7 @@ def maindown(url,model):
                     a = d.get_result()
                     filenum = filenum + a
             else:
-                if sleepnum == 7 and filenum:
+                if sleepnum == 15 and filenum:
                     print(model+'下载完成')
                     gLock.acquire()
                     status[model] = 0
@@ -195,13 +197,13 @@ def maindown(url,model):
                     del_file(file)
                     request_post(model,'合并完成网址是mfc.lxgd.design/'+model+'/'+str(now) +'.ts')
                     break
-                elif sleepnum == 7 and not filenum:
+                elif sleepnum == 15 and not filenum:
                     print('可能Temporarily Away')
                     break
                 else:
                     print('数字为'+str(sleepnum))
                     sleepnum  = sleepnum + 1
-                    time.sleep(2)
+                    time.sleep(1.5)
     else:
         print('m3u8连接失败')        
 
@@ -215,7 +217,7 @@ class download(threading.Thread):#下载程序
         while True:
             if not self.que.empty():
                 url = self.que.get()
-                r = request_get(url)
+                r = request_get(url,5,3)
                 if r.status_code == 200:
                     num = findnum(url)
                     with open(self.file + str(num)  + '.ts','wb') as a:
@@ -237,11 +239,11 @@ def findnum(url):#获取TS链接的序号num
     num = int(num)
     return num
 
-def request_get(url):
+def request_get(url,timeouts,fq):
     s = requests.Session()
-    s.mount('http://', HTTPAdapter(max_retries=5))
-    s.mount('https://', HTTPAdapter(max_retries=5))
-    r = s.get(url, timeout=5)
+    s.mount('http://', HTTPAdapter(max_retries=fq))
+    s.mount('https://', HTTPAdapter(max_retries=fq))
+    r = s.get(url,headers=header,timeout=timeouts)
     return r
 
 def request_post(model,text):#使用server酱推送微信
@@ -254,12 +256,17 @@ def request_post(model,text):#使用server酱推送微信
 if __name__ == '__main__':
     sys = platform.system()
     if sys == "Windows":
-        temppath = 'e:/temp/'
-        savepath = 'e:/video/'
+        temppath = 'f:/temp/'
+        savepath = 'f:/video/'
     else:
         temppath = '/root/temp/'
-        savepath = '/www/wwwroot/mfc.lxgd.design/'
-    remote = 'wss://xchat72.myfreecams.com/fcsl'
+        savepath = '/root/video/'
+    remote = 'wss://xchat61.myfreecams.com/fcsl'
+    header={
+	    'Origin': 'https://m.myfreecams.com',
+	    'Referer': 'https://m.myfreecams.com/chats',
+	    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+    }
     api = "https://sc.ftqq.com/SCU84034T4390e074a26e7a7ed66427692ac0a7b65e48f3b1d85ee.send?text="#server酱处获取
     creat_file(temppath)
     creat_file(savepath)
@@ -280,4 +287,5 @@ if __name__ == '__main__':
         logging.info('Quit.')
     
     except Exception as e:
+        print(e)
         request_post('抛出异常','')
